c6d55cbc328809aa37d739b22e87be8dcb5e5283
beambitiousnow <cd2667514@gmail.com>
2022-05-18 17:57:44 +0900

    add get_song

--- api_prac/spo/get_song_recommen.py
+++ api_prac/spo/get_song_recommen.py
@@ -1,12 +1,12 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import pprint
 
 def get_song_recommen (sp, id, genres, track_id): #(sp, artist_name, artist_id, genres, tracks_id)
 
     #아티스트 아이디와 장르, 트랙입력하면 유사한 음악 추천해주는 함수  => seed_tracks 수정필요/tracks > 
     rec = sp.recommendations(seed_artists=[id], seed_genres=[genres], seed_tracks=[track_id], limit=5)
     for track in rec['tracks']:
-        recommend = (track['artists'][0]['name'], track['name']) #+앨범아트코드
+        recommend_song = (track['artists'][0]['name'], track['name'], track['album']['images'][0]['url'])
+        #cover_art = (track['tracks']['items'][0]['album']['images'][0]['url']'])
     
-    return recommend
+    return recommend_song
--- api_prac/spo/run.py
+++ api_prac/spo/run.py
@@ -2,28 +2,15 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pprint
 from get_spotify_access import get_spotify_credentials
-from get_albums_details import get_album_details
+from get_search import get_search
 from get_album_art import get_album_art
 from get_song_recommen import get_song_recommen
-
 from spotify_credentials import CLIENT_ID, CLIENT_SECRET
 
 sp = get_spotify_credentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+search_for = '소녀시대'
 
-artist_name = '소녀시대'
-album_names, album_name_uri_dict, album_img_url_dict, artist_uri = get_album_details(sp=sp, artist_name=artist_name)
-#pprint.pprint(album_names)
-#print(album_img_url_dict)
+artist_id, artist_name, tracks_id, tracks_name, tracks_image, search_result = get_search(sp=sp, search=search_for)
+#pprint.pprint(list(search_result))
 
-print()
-#serach함수 형식 -> name:artist, limit=(int), type='album,track'   (= artist가 포함된 album, track정보)
-#정보 검색, limit=1로하여 가장 상단의 아티스트만
-#result = sp.search(artist_name, limit=1, type='track')
-#pprint.pprint(result)
-
-#album_names = album_names[0]
-
-#get_album_art(album_name=album_names, album_img_url_dict=album_img_url_dict,album_cover_path='./')
-#print(get_album_art)
-
-#get_song_recommen(sp=sp, artist_id=artist_uri,genres= genres, track_id = like_track_id )
+get_song_recommen(sp=sp, artist_id=artist_id, genres= genres, track_id = tracks_id )
